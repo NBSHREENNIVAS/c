d9881f57aa66d81a8d49ae98965ff9cd16544db0
NB SP <nbshreennivaspradhan@gmail.com>
2024-12-02 13:45:59 +0530

    commit

---
+++ hary_loop.c
@@ -0,0 +1,28 @@
+// DO WHILE LOOP
+// #include <stdio.h>
+// int main()
+// {
+//     int num, index = 0;
+//     printf("Enter a number:");
+//     scanf("%d", &num);
+
+//     do
+//     {
+//         printf("%d\n", index + 1);
+//         index = index + 1;
+
+//     } while (index < num);
+
+//     return 0;
+
+// ONLY WHILE LOOP
+#include<stdio.h>
+int main(){
+int i = 0; 
+while (i < 66)
+{
+    printf("%d\n", i);
+    i = i + 1;
+} 
+    return 0;
+}
---
+++ hw.c
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+// #include<math.h>
+// int fun(int x,int y){
+//     printf(pow(x,y));
+//     return 0;
+// }
+// int main(){
+//     int x,y;
+//     printf("input the number to be powered:");
+//     scanf("%d",&x);
+//     printf("input the power of the number:");
+//     scanf("%d",&y);
+//     fun(x,y);
+// }
+
